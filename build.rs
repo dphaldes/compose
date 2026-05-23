9030f7f3043635c3d68defeb5e43fcaef8e7e3e0
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2026-05-22 19:59:55 -0700

    chore: remove extra includes

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ use cxx_qt_build::{CxxQtBuilder, QmlModule};
 
 fn main() {
     CxxQtBuilder::new_qml_module(QmlModule::new("org.kde.compose").qml_file("src/qml/Main.qml"))
-        .include_dir("src/include")
         .file("src/preview.rs")
         .build();
 }
